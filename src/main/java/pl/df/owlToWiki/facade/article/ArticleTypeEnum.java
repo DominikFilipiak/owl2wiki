96c18b9b2c5a0d95e29359be4fce746518917e8e
Dominik <skrzatanek@gmail.com>
2014-04-16 13:59:11 +0200

    Minor

---
+++ src/main/java/pl/df/owlToWiki/facade/article/ArticleTypeEnum.java
@@ -0,0 +1,10 @@
+package pl.df.owlToWiki.facade.article;
+
+/**
+ * User: dominikfilipiak
+ * Date: 13/03/2014
+ * Time: 21:45
+ */
+public enum ArticleTypeEnum {
+    ARTICLE, TEMPLATE, CATEGORY
+}
